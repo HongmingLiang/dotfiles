31f39456dabd3a91eae3005e79072c1f399a0e61
HongmingLiang <hm.liang.dev@outlook.com>
2025-11-29 21:54:56 +0800

    `LazyVim`: new keymaps

--- dotfiles/nvim/lua/config/keymaps.lua
+++ dotfiles/nvim/lua/config/keymaps.lua
@@ -6,17 +6,23 @@
 local map = vim.keymap.set
 local unmap = vim.keymap.del
 
--- override <C-_> from "open Terminal" to "Toggle Comment"
--- and map <C-t> to open floating Terminal
-unmap({ "n", "t" }, "<c-_>", { desc = "which_key_ignore" })
-map("v", "<C-_>", "gc", { desc = "Toggle Comment", remap = true })
-map("n", "<C-_>", "gcc", { desc = "Toggle Comment", remap = true })
-map({ "n", "t" }, "<C-t>", function()
-  Snacks.terminal(nil, { cwd = LazyVim.root() })
-end, { desc = "Terminal (Root Dir)" })
-
 -- fast escape to normal mode
 map("i", "jk", "<Esc>l", { desc = "Escape to normal mode" })
+-- fast quit
+map({ "n", "x" }, "qq", "<CMD>:q<CR>", { desc = "Quit" })
+-- fast quit all
+map({ "n", "x" }, "Q", "<CMD>:qa<CR>", { desc = "Quit all" })
+
+-- fast close buffer
+map("n", "W", "<leader>bd", { desc = "Close Buffer", remap = true })
+
+---- Alt related keys----
+
+-- set alt to move cursor
+map("i", "<A-h>", "<Left>", { desc = "Cursor Move Left", noremap = true })
+map("i", "<A-j>", "<Down>", { desc = "Cursor Move Down", noremap = true })
+map("i", "<A-k>", "<Up>", { desc = "Cursor Move Up", noremap = true })
+map("i", "<A-l>", "<Right>", { desc = "Cursor Move Right", noremap = true })
 
 -- set alt to move to window
 map("n", "<A-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
@@ -35,7 +41,21 @@ unmap("v", "<A-k>", { desc = "Move Up" })
 map("v", "<C-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
 map("v", "<C-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
 
+---- Control related keys ----
+
+-- override <C-_> from "open Terminal" to "Toggle Comment"
+-- and map <C-t> to open floating Terminal
+unmap({ "n", "t" }, "<c-_>", { desc = "which_key_ignore" })
+map("v", "<C-_>", "gc", { desc = "Toggle Comment", remap = true })
+map("n", "<C-_>", "gcc", { desc = "Toggle Comment", remap = true })
+map({ "n", "t" }, "<C-t>", function()
+  Snacks.terminal(nil, { cwd = LazyVim.root() })
+end, { desc = "Terminal (Root Dir)" })
+
 --- Select all
 map("n", "<C-a>", "ggVG", { desc = "Select All" })
+-- undo
+map("i", "<C-z>", "<Esc>ui", { desc = "Undo" })
+map("n", "<C-z>", "u", { desc = "Undo" })
 
 ----- end of custom configuration -----
