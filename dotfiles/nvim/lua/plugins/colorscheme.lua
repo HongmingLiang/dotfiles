f00f8097f1f96663046cced9ce4b82698a0ddf6a
HongmingLiang <hm.liang.dev@outlook.com>
2025-12-09 22:42:24 +0800

    change line number color to overlay1; delete example.lua; update some LazyVim plugins

--- dotfiles/nvim/lua/plugins/colorscheme.lua
+++ dotfiles/nvim/lua/plugins/colorscheme.lua
@@ -10,14 +10,28 @@ return {
     opts = {
       flavour = "mocha",
       background = { light = "latte", dark = "mocha" },
+      -- transparent
       transparent_background = true,
       float = {
         transparent = true,
       },
+      -- override colors
+      custom_highlights = function(colors)
+        return {
+          LineNr = { fg = colors.overlay1 },
+        }
+      end,
+      -- integrations
       integrations = {
         rainbow_delimiters = true,
       },
       auto_integrations = true,
+    },
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      code = { border = "thick" },
     },
   },
   {
