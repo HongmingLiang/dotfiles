31eb75050b9a9222fa9b096532ec9a54a3076f68
HongmingLiang <hm.liang.dev@outlook.com>
2026-03-07 01:54:22 +0800

    bashrc: 1) add opencode alias and 2) reformat

--- dotfiles/bash/bashrc.d/bashrc_tools.sh
+++ dotfiles/bash/bashrc.d/bashrc_tools.sh
@@ -1,38 +1,38 @@
 #!/usr/bin/env bash
 
 # --- editor ---
-if command -v nvim >/dev/null 2>&1; then
-	export EDITOR=nvim
-	alias vvim="$(which vim)"
-	alias vim='nvim'
+if [ "$(id -u)" -ne 0 ] && command -v nvim > /dev/null 2>&1; then
+  export EDITOR=nvim
+  alias vvim="$(which vim)"
+  alias vim='nvim'
 else
-	export EDITOR=vim
-	export VIMINIT="source $XDG_CONFIG_HOME/vim/vimrc"
+  export EDITOR=vim
+  export VIMINIT="source $XDG_CONFIG_HOME/vim/vimrc"
 fi
 alias v="$EDITOR"
 
 # shell prompt
-if command -v starship >/dev/null 2>&1; then # starship
-	export STARSHIP_CONFIG="$XDG_CONFIG_HOME/starship/starship.toml"
-	eval "$(starship init bash)"
-elif command -v git >/dev/null 2>&1; then # PS1 with Git branch info
-	PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;33m\]$(git branch 2>/dev/null | sed -n "s/^\* \(.*\)/ (\1)/p")\[\033[00m\]\$ '
+if command -v starship > /dev/null 2>&1; then # starship
+  export STARSHIP_CONFIG="$XDG_CONFIG_HOME/starship/starship.toml"
+  eval "$(starship init bash)"
+elif command -v git > /dev/null 2>&1; then # PS1 with Git branch info
+  PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;33m\]$(git branch 2>/dev/null | sed -n "s/^\* \(.*\)/ (\1)/p")\[\033[00m\]\$ '
 else
-	PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+  PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 fi
 
 # colorful output for ls and grep
 if [ -x /usr/bin/dircolors ]; then
-	test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-	alias ls='ls --color=auto'
-	alias grep='grep --color=auto'
-	alias fgrep='fgrep --color=auto'
-	alias egrep='egrep --color=auto'
+  test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+  alias ls='ls --color=auto'
+  alias grep='grep --color=auto'
+  alias fgrep='fgrep --color=auto'
+  alias egrep='egrep --color=auto'
 fi
 
 # eza
-if command -v eza >/dev/null 2>&1; then
-	alias ls='eza --group --icons --git --group-directories-first'
+if command -v eza > /dev/null 2>&1; then
+  alias ls='eza --group --icons --git --group-directories-first'
 fi
 
 # aliases for ls
@@ -41,64 +41,70 @@ alias la='ls -A'
 alias l='ls -hF'
 
 # fzf
-if command -v fzf >/dev/null 2>&1; then
-	eval "$(fzf --bash)" # Set up fzf key bindings and fuzzy completion
-	command -v tmux &>/dev/null && alias ftsw='tmux switch-client -t $(tmux ls -F "#{session_name}" | fzf)'
-	__falias_widget() { # interactively search alias and place it on command line
-		local cmd
-		cmd=$(alias | sed "s/^alias //" | fzf --prompt="Alias> " | awk -F'=' '{print $1}')
-		if [[ -n "$cmd" ]]; then
-			READLINE_LINE="$cmd "
-			READLINE_POINT=${#READLINE_LINE}
-		fi
-	}
-	bind -x '"\C-x\C-a": __falias_widget'
+if command -v fzf > /dev/null 2>&1; then
+  eval "$(fzf --bash)" # Set up fzf key bindings and fuzzy completion
+  command -v tmux &> /dev/null && alias ftsw='tmux switch-client -t $(tmux ls -F "#{session_name}" | fzf)'
+  __falias_widget() { # interactively search alias and place it on command line
+    local cmd
+    cmd=$(alias | sed "s/^alias //" | fzf --prompt="Alias> " | awk -F'=' '{print $1}')
+    if [[ -n "$cmd" ]]; then
+      READLINE_LINE="$cmd "
+      READLINE_POINT=${#READLINE_LINE}
+    fi
+  }
+  bind -x '"\C-x\C-a": __falias_widget'
 fi
 
 # bat: better `cat`
-if command -v bat >/dev/null 2>&1; then
-	export BAT_CONFIG_PATH="$XDG_CONFIG_HOME/bat/config"
-	export BAT_CONFIG_DIR="$XDG_CONFIG_HOME/bat"
-	alias ccat="$(which cat)" # keep original cat accessible via ccat
-	alias cat='bat --paging=never'
-	alias catp='bat --paging=always'
-	batdiff() { # show git diff with bat
-		git diff --name-only --relative --diff-filter=d -z | xargs -0 bat --diff
-	}
-	bathelp() { # help command with bat formatting
-		"$@" --help 2>&1 | bat --plain --language=help
-	}
-	batail() { # tail -f with bat syntax highlighting
-		tail -f "$@" | bat --paging=never -l log --style=plain
-	}
+if command -v bat > /dev/null 2>&1; then
+  export BAT_CONFIG_PATH="$XDG_CONFIG_HOME/bat/config"
+  export BAT_CONFIG_DIR="$XDG_CONFIG_HOME/bat"
+  alias ccat="$(which cat)" # keep original cat accessible via ccat
+  alias cat='bat --paging=never'
+  alias catp='bat --paging=always'
+  batdiff() { # show git diff with bat
+    git diff --name-only --relative --diff-filter=d -z | xargs -0 bat --diff
+  }
+  bathelp() { # help command with bat formatting
+    "$@" --help 2>&1 | bat --plain --language=help
+  }
+  batail() { # tail -f with bat syntax highlighting
+    tail -f "$@" | bat --paging=never -l log --style=plain
+  }
 fi
 
 # dust: better `du`
-if command -v dust >/dev/null 2>&1; then
-	for i in {1..5}; do
-		alias "du$i"="dust -d $i -p"
-	done
-	alias du="dust -p"
+if command -v dust > /dev/null 2>&1; then
+  for i in {1..5}; do
+    alias "du$i"="dust -d $i -p"
+  done
+  alias du="dust -p"
 else
-	for i in {1..5}; do
-		alias "du$i"="du -hd $i"
-	done
-	alias du="du -h"
+  for i in {1..5}; do
+    alias "du$i"="du -hd $i"
+  done
+  alias du="du -h"
 fi
 
 # gping
-if command -v gping >/dev/null 2>&1; then
-	alias pping='$(which ping)' # keep original ping accessible via pping
-	alias ping='gping'
+if command -v gping > /dev/null 2>&1; then
+  alias pping='$(which ping)' # keep original ping accessible via pping
+  alias ping='gping'
 fi
 alias ping='ping -c 5'
 
 # yazi shell wrapper for changing directory: https://yazi-rs.github.io/docs/quick-start
 function y() {
-	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-	yazi "$@" --cwd-file="$tmp"
-	IFS= read -r -d '' cwd <"$tmp"
-	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
-	rm -f -- "$tmp"
+  local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+  yazi "$@" --cwd-file="$tmp"
+  IFS= read -r -d '' cwd < "$tmp"
+  [ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+  rm -f -- "$tmp"
 }
 alias yy='yazi'
+
+# opencode
+if command -v opencode > /dev/null 2>&1; then
+  alias oc='opencode'
+  alias oc-c="opencode --continue"
+fi
