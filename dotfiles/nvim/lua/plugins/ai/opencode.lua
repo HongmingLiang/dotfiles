cb09be91d7d99c4543fae2f3820545f580ec2703
HongmingLiang <hm.liang.dev@outlook.com>
2026-04-13 17:26:07 +0800

    nvim: opencode with integration

--- dotfiles/nvim/lua/plugins/ai/opencode.lua
+++ dotfiles/nvim/lua/plugins/ai/opencode.lua
@@ -1,15 +1,65 @@
 vim.pack.add({ "https://github.com/nickjvandyke/opencode.nvim.git" })
 
-vim.keymap.set({ "n", "x" }, "<leader>aa", function()
+local opencode_window = "opencode"
+local opencode_cmd = "opencode --port"
+
+local function tmux_window_exists()
+	local windows = vim.fn.systemlist({ "tmux", "list-windows", "-F", "#W" })
+	return vim.tbl_contains(windows, opencode_window)
+end
+
+vim.g.opencode_opts = {
+	server = {
+		start = function()
+			if not tmux_window_exists() then
+				vim.fn.system({ "tmux", "new-window", "-d", "-n", opencode_window, opencode_cmd })
+			end
+		end,
+		stop = function()
+			if tmux_window_exists() then
+				vim.fn.system({ "tmux", "kill-window", "-t", opencode_window })
+			end
+		end,
+		toggle = function()
+			if tmux_window_exists() then
+				vim.fn.system({ "tmux", "select-window", "-t", opencode_window })
+			else
+				vim.fn.system({ "tmux", "new-window", "-n", opencode_window, opencode_cmd })
+			end
+		end,
+	},
+}
+
+local map = vim.keymap.set
+
+map({ "n" }, "<leader>aa", function()
+  require("opencode").ask("@buffer: ", { submit = true })
+end, { desc = "Ask opencode (buffer)" })
+
+map({ "x" }, "<leader>aa", function()
   require("opencode").ask("@this: ", { submit = true })
 end, { desc = "Ask opencode…" })
-vim.keymap.set({ "n", "x" }, "<leader>as", function()
+
+map({ "n" }, "<leader>at", function()
+  require("opencode").toggle()
+end, { desc = "Toggle opencode" })
+
+map({ "n", "x" }, "<leader>as", function()
   require("opencode").select()
 end, { desc = "Execute opencode action…" })
 
-vim.keymap.set({ "x" }, "go", function()
+map({ "x" }, "go", function()
   return require("opencode").operator("@this ")
 end, { desc = "Add range to opencode", expr = true })
-vim.keymap.set("n", "go", function()
+
+map("n", "go", function()
   return require("opencode").operator("@this ") .. "_"
 end, { desc = "Add line to opencode", expr = true })
+
+map("n", "<C-A-u>", function()
+  require("opencode").command("session.half.page.up")
+end, { desc = "Scroll opencode up" })
+
+map("n", "<C-A-d>", function()
+  require("opencode").command("session.half.page.down")
+end, { desc = "Scroll opencode down" })
