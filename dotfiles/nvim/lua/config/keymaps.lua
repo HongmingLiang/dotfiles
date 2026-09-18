31381029b872800aed282f8dbd7e2b6ebbe2361a
HongmingLiang <hm.liang.dev@outlook.com>
2026-09-18 16:02:27 +0800

    fix(nvim): silence line move keymaps
    Add silent to C-j/C-k visual move mappings to avoid noisier emacs-style
    echo output.

--- dotfiles/nvim/lua/config/keymaps.lua
+++ dotfiles/nvim/lua/config/keymaps.lua
@@ -33,8 +33,8 @@ map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, 
 map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
 
 -- move lines or blocks up/down
-map("v", "<C-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
-map("v", "<C-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
+map("v", "<C-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down", silent = true })
+map("v", "<C-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up", silent  = true })
 -- map("n", "<C-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
 -- map("n", "<C-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
 -- map("i", "<C-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
